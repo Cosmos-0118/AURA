b619d93125c811ed2d025f8f2b458042b7aab955
Dhanush S <dhanush13244@gmail.com>
2026-09-24 06:20:39 +0530

    chore: remove outdated files including Concept.md, PowerShell script, and social media icons

--- fix.ps1
+++
@@ -1,2 +0,0 @@
-(Get-Content -Path scripts\windows\aura.ps1) -replace '\$pid\b', '$procId' | Set-Content -Path scripts\windows\aura.ps1
-(Get-Content -Path scripts\windows\aura.ps1) -replace '\$pidFile\b', '$procIdFile' | Set-Content -Path scripts\windows\aura.ps1
